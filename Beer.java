4dac71b13da2e29ba792a8bde9288f1b5dad5f3b
zhmiller <zhmiller@oakland.edu>
2016-02-04 21:47:47 -0500

    Final iteration of Beer file

--- Beer.java
+++ Beer.java
@@ -9,6 +9,23 @@ package beer;
  *
  * @author Inspiron
  */
+public class Beer {
 
-
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        String s=" ";
+for(int x=99; x>0; x--)
+{
+System.out.println(x+" bottles of beer on the wall, "+x+" bottles of beer");
+System.out.println("Take one down, pass it around, "+(x-1)+" bottles of beer on the wall \n");
+}
+System.out.print("Go to the store, buy some more, ");
+System.out.println("99 bottles of beer on the wall.\n");
+System.exit(0);
+} 
+    }
+    
 
